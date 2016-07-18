77b76e161882f0a570709a0ba742e39eca9f42c7
sunhong <sunhong@ucfgroup.com>
2016-07-18 10:31:15 +0800

    Initial Commit

---
+++ AllowPCLoginIn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AllowPCLoginIn
+//
+//  Created by sunhong on 16/7/18.
+//  Copyright © 2016年 sunhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
